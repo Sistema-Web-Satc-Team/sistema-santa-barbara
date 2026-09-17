aee58cbb5e5c7b1bc0beb3ad542c31fd06d8facf
Jviiniciuss <joaovinciusmv0@gmail.com>
2026-09-16 22:33:25 -0300

    Update AppRoutes.tsx

--- santa-barbara-web/src/application/routes/AppRoutes.tsx
+++ santa-barbara-web/src/application/routes/AppRoutes.tsx
@@ -2,6 +2,8 @@ import { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router";
 
 const Components = lazy(() => import('@/application/pages/Components'));
+const ConvidarMembros = lazy(() => import('@/application/pages/ConvidarMembros'));
+const AceitarConvite = lazy(() => import('@/application/pages/AceitarConvite'));
 
 function AppRoutes() {
     return(
@@ -9,6 +11,8 @@ function AppRoutes() {
             <Suspense fallback={<div>Carregando...</div>}>
                 <Routes>
                     <Route path="/dev/components" element={<Components/>} />
+                    <Route path="/convidar-membros" element={<ConvidarMembros />} />
+                    <Route path="/convite/:idConvite" element={<AceitarConvite />} />
                 </Routes>
             </Suspense>
         </BrowserRouter>
