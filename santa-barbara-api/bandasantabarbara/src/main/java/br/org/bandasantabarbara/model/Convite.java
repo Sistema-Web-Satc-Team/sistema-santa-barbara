7b36056b00dd5b8e9c3f2c1335f502f7a8446a71
LaurenceFluciano <laurencefluciano@gmail.com>
2026-09-21 16:03:15 -0300

    Adicionar entidade de convite.

---
+++ santa-barbara-api/bandasantabarbara/src/main/java/br/org/bandasantabarbara/model/Convite.java
@@ -0,0 +1,49 @@
+package br.org.bandasantabarbara.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.Fetch;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.UUID;
+
+@Entity
+public class Convite {
+
+    @Getter
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Getter
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Membro membro;
+
+    @Getter
+    @Column(name = "data_convite_criacao", nullable = false)
+    private Instant dataCriacaoConvite;
+
+    @Getter
+    @Column(name = "data_convite_expiracao", nullable = false)
+    private Instant dataExpiracaoConvite;
+
+    @Getter @Setter
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private ConviteStatus status;
+
+
+    protected Convite() {
+        this.dataCriacaoConvite = Instant.now();
+        this.dataExpiracaoConvite = this.dataCriacaoConvite.plus(7, ChronoUnit.DAYS);
+    }
+
+    public Convite(Membro membro) {
+        this();
+        this.membro = membro;
+    }
+
+
+}
---
+++ santa-barbara-api/bandasantabarbara/src/main/java/br/org/bandasantabarbara/model/ConviteStatus.java
@@ -0,0 +1,10 @@
+package br.org.bandasantabarbara.model;
+
+public enum ConviteStatus {
+    ENVIADO,
+    REENVIADO,
+    ACEITO,
+    REJEITADO,
+    EXPIRADO,
+    FALHOU
+}
