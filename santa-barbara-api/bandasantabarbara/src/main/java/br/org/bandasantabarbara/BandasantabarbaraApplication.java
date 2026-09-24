4a0b493faf9d4dd5bb6927c8d785fbc601744fe3
LaurenceFluciano <laurencefluciano@gmail.com>
2026-09-24 15:17:21 -0300

    fix: adicionar jdbc no application.properties.

--- santa-barbara-api/bandasantabarbara/src/main/java/br/org/bandasantabarbara/BandasantabarbaraApplication.java
+++ santa-barbara-api/bandasantabarbara/src/main/java/br/org/bandasantabarbara/BandasantabarbaraApplication.java
@@ -13,8 +13,6 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class BandasantabarbaraApplication {
 
 	public static void main(String[] args) {
-		// Dotenv dotenv = Dotenv.load();
-		// dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 
 		SpringApplication.run(BandasantabarbaraApplication.class, args);
 	}
