1f8bc3a7d9a1df12dfd253cf78c9f6cecf55a2d3
LaurenceFluciano <laurencefluciano@gmail.com>
2026-09-24 15:12:24 -0300

    Remover código que tenta ler dotenv, uma vez que variaveis de ambientes nao sao mais definidas aqui.

--- santa-barbara-api/bandasantabarbara/src/main/java/br/org/bandasantabarbara/BandasantabarbaraApplication.java
+++ santa-barbara-api/bandasantabarbara/src/main/java/br/org/bandasantabarbara/BandasantabarbaraApplication.java
@@ -1,6 +1,5 @@
 package br.org.bandasantabarbara;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
@@ -14,8 +13,8 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class BandasantabarbaraApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+		// Dotenv dotenv = Dotenv.load();
+		// dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 
 		SpringApplication.run(BandasantabarbaraApplication.class, args);
 	}
